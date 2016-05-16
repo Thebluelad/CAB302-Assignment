dc10bbf1a4db50592449f40e921d04c5fccb649b
thematthewest <matt@thebluelad.local>
2016-05-16 12:58:45 +1000

    Added the passenger test class

---
+++ AircraftSimulator/src/asgn2Tests/PassengerTests.java
@@ -0,0 +1,14 @@
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PassengerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
