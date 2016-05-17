472bb2b5060d1a50f6d9bff9f7fe73076cd48e72
thematthewest <matt@thebluelad.local>
2016-05-18 00:50:22 +1000

    Added some more passenger tests

--- AircraftSimulator/src/asgn2Tests/PassengerTests.java
+++ AircraftSimulator/src/asgn2Tests/PassengerTests.java
@@ -104,4 +104,124 @@ public class PassengerTests {
 		//Should throw the exception because the departureTime is less than the cancellationTime
 		test.cancelSeat(cancellationTime);
 	}
+	
+	@Test
+	public void confirmSeatIsNew() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.isNew() == false);
+	}
+	
+	@Test
+	public void confirmSeatIsConfirmed() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.isConfirmed() == true);
+	}
+	
+	@Test
+	public void confirmSeatConfirmationTime() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.getConfirmationTime() == confirmationTime);
+	}
+	
+	@Test
+	public void confirmSeatDepartureTime() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.getDepartureTime() == departureTime);
+	}
+	
+	@Test
+	public void confirmSeatIsQueued() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.isQueued() == false);
+	}
+	
+	@Test
+	public void confirmSeatExitQueueTime() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		assertTrue(test.getExitQueueTime() == confirmationTime);
+	}
+	
+	@Test (expected = asgn2Passengers.PassengerException.class)
+	public void confirmSeatExceptionThrowIsConfirmed() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+		test.confirmSeat(confirmationTime, departureTime);
+	}
+	
+	@Test (expected = asgn2Passengers.PassengerException.class)
+	public void confirmSeatExceptionThrowIsRefused() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		int refusalTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.refusePassenger(refusalTime);
+		
+		//Should fail because the passenger should be refused
+		test.confirmSeat(confirmationTime, departureTime);
+	}
+	
+	@Test (expected = asgn2Passengers.PassengerException.class)
+	public void confirmSeatExceptionThrowIsFlown() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = 2;
+		Economy test = new Economy(bookingTime, departureTime);
+		test.flyPassenger(departureTime);
+		
+		//Should fail because the passenger is flown
+		test.confirmSeat(confirmationTime, departureTime);
+	}
+	
+	@Test (expected = asgn2Passengers.PassengerException.class)
+	public void confirmSeatExceptionThrowConfirmationTime() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 3;
+		int confirmationTime = -1;
+		Economy test = new Economy(bookingTime, departureTime);
+		
+		test.confirmSeat(confirmationTime, departureTime);
+	}
+	
+	@Test (expected = asgn2Passengers.PassengerException.class)
+	public void confirmSeatExceptionThrowDepartureConfirmation() throws PassengerException {
+		int bookingTime = 1;
+		int departureTime = 2;
+		int confirmationTime = 3;
+		Economy test = new Economy(bookingTime, departureTime);
+		
+		test.confirmSeat(confirmationTime, departureTime);
+	}
+	
+	@Test
+	public void flyPassenger() throws PassengerException {
+		
+	}
 }
